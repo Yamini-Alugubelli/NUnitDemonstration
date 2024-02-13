723b50a2c192b7caa5a4008a58d9813bc8350e31
Yamini Alugubelli <yaminireddyallugubeli.70@gmail.com>
2024-02-12 19:16:45 -0500

    Add project files.

---
+++ TestClass1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnitDemonstration;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace TestClass1
+{
+    //SENG8041_YaminiAlugubeli_8991450_lab2
+    public class Class1
+    {
+        public class TriangleTest2
+        {
+            [Test]
+            public void ValidTraiangle_Input60and60and60_OutputValidTrialngle()
+            {
+                int firstAngle = 60;
+                int secondAngle = 60;
+                int thirdAngle = 60;
+
+                String excepted = "The triangle is valid.";
+                String actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+                ClassicAssert.AreEqual(excepted, actual);
+            }
+        }
+    }
+}
